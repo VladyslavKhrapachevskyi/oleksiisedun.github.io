7a1fc78ba897fd5904cdfaa68758e17194e7c1ba
Oleksii Sedun <oleksiisedun@gmail.com>
2022-06-03 21:00:01 +0300

    Upd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My personal React Site
+          My personal React Site ^_^
         </p>
         <a
           className="App-link"
